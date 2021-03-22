d1bad7f4a50504e8511cd783bc2970ec1212ee29
Elias <elias@baez.us>
2021-03-22 16:38:31 -0400

    Got very basic splash page to load.

--- frontend/baez3/src/pages/Splash.js
+++ frontend/baez3/src/pages/Splash.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Splash = () => {
   return (
-    <div> 
-      <h1>baez.us</h1>
+    <div className = "columns">
+      <div className="column is-full">
+            <h1 className = "title is-1">baez.us</h1>
+      </div>
     </div>
   );
 }
